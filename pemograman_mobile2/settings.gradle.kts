6dfb7aff4b4d3a3e2a6d6fe895589e774c6b7c75
dheadwiadelia <dheadwiadelia@gmail.com>
2024-03-22 17:31:57 +0700

    Pemograman Mobile 2

---
+++ pemograman_mobile2/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "pemograman_mobile2"
+include(":app")
+ 
